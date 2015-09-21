980def582506cc1f23a14960b047c868d5a6e038
Conner Smith <connerpsmith@gmail.com>
2015-09-21 16:44:42 -0500

    stubbed out remaining pages and controllers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ActiveRecord::Base
 	validates :first_name, :last_name, :sex, :dob, :status, presence: true
 
-	has_one :address, :system, :insurance
+	has_one :system, :insurance
+	has_many :addresses
 	belongs_to :caseworker, :billing_interval, :region
 	accepts_nested_attributes_for :address, :system
 end
