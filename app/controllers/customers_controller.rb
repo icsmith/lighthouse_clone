e73291681e5655344096a9ac732c0d66e11ced77
Ian Smith <ianc.smith@live.com>
2015-09-23 15:23:56 -0500

    customer index page links to show page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,11 @@ class CustomersController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
-
+		@customers = Customer.all
 	end
 
 	def show
-
+		@customer = Customer.find(params[:id])
 	end
 
 	def new
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,12 @@
 class Customer < ActiveRecord::Base
 	validates :first_name, :last_name, :sex, :dob, :status, presence: true
 
-	has_one :system, :insurance
+	has_one :system
+	has_one :insurance
 	has_many :addresses
-	belongs_to :caseworker, :billing_interval, :region
-	accepts_nested_attributes_for :address, :system
+	belongs_to :caseworker
+	belongs_to :billing_interval
+	belongs_to :region
+	accepts_nested_attributes_for :addresses
+	accepts_nested_attributes_for :system
 end
